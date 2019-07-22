686ae0a5bd70409b903952020e94fed3c3eac401
Jappie Klooster <jappieklooster@hotmail.com>
2019-07-22 22:57:28 +0200

    Add basic movememtn

--- default.nix
+++ default.nix
@@ -1,13 +1,14 @@
-{ mkDerivation, base, bytestring, hpack, lens, sdl2, stdenv }:
+{ mkDerivation, base, bytestring, hpack, lens, sdl2, stdenv, vector
+}:
 mkDerivation {
   pname = "template";
   version = "1.0.0";
   src = ./.;
   isLibrary = true;
   isExecutable = true;
-  libraryHaskellDepends = [ base bytestring lens sdl2 ];
+  libraryHaskellDepends = [ base bytestring lens sdl2 vector ];
   libraryToolDepends = [ hpack ];
-  executableHaskellDepends = [ base bytestring lens sdl2 ];
+  executableHaskellDepends = [ base bytestring lens sdl2 vector ];
   preConfigure = "hpack";
   license = stdenv.lib.licenses.mit;
 }
