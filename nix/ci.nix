30d4d963d6d71f5f7fbf60fc13ab3ae213134993
Jappie Klooster <jappieklooster@hotmail.com>
2020-11-23 22:38:27 +0100

    Disable 810

--- nix/ci.nix
+++ nix/ci.nix
@@ -4,7 +4,7 @@
 {
   # https://github.com/NixOS/nixpkgs/blob/master/pkgs/top-level/haskell-packages.nix#L47
   ghc-def = import ../default.nix { inherit pkgs; };
-  ghc-810 = import ../default.nix {compiler = "ghc8102"; inherit pkgs;};
+  # ghc-810 = import ../default.nix {compiler = "ghc8102"; inherit pkgs;};
   # ghc-901 = import ../default.nix {compiler = "ghc901"; inherit pkgs;};
   # test = pkgs.nixosTest nix/test.nix;
 }
