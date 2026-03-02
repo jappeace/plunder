00404537573ef377a8516aeeda441d42ff41662b
Jappie Klooster <jappieklooster@hotmail.com>
2026-03-02 21:46:05 +0100

    upgrade nixpkgs

--- nix/hpkgs.nix
+++ nix/hpkgs.nix
@@ -5,5 +5,8 @@ pkgs.haskellPackages.override {
   overrides = hnew: hold: {
     game13 = hnew.callCabal2nix "game13" ../. { };
     reflex-sdl2 = hnew.callCabal2nix "reflex-sdl2" sources.reflex-sdl2 { };
+    sdl2-image = pkgs.haskell.lib.overrideCabal
+      (pkgs.haskell.lib.markUnbroken hold.sdl2-image)
+      { __onlyPropagateKnownPkgConfigModules = true; };
   };
 }
